7aaf9c29a4e888ef82ff204cc276ed652d3a645f
Md. Akhtar Uzzaman <akhtaruzzaman05@gmail.com>
2025-02-27 11:11:44 +0600

    added avro writer

---
+++ js/unicodemath.js
@@ -0,0 +1,22 @@
+﻿function bdmath(expression, precision) {
+    try {
+        let numericExpression = replaceBantoEng(expression);
+        let result = eval(numericExpression);
+
+        if (precision !== undefined) {
+            result = parseFloat(result).toFixed(parseInt(replaceBantoEng(precision)));
+        }
+
+        return replaceEngToBan(result.toString());
+    } catch {
+        return '<span style="color:red">NaN</span>';
+    }
+}
+
+function replaceBantoEng(str) {
+    return str.replace(/[০-৯]/g, d => "০১২৩৪৫৬৭৮৯".indexOf(d));
+}
+
+function replaceEngToBan(str) {
+    return str.replace(/\d/g, d => "০১২৩৪৫৬৭৮৯"[d]);
+}
